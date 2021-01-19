3ed9bd6b928d6a689abcc2c445ef276851f1efdc
pinktoothbrush13 <jinandreiparcia13@gmail.com>
2021-01-19 17:06:12 +0800

    remove gachi
    refactor button names
    
    added buttons in code

--- app/src/main/java/mcm/edu/ph/parcia_calcusaandroid/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/parcia_calcusaandroid/MainActivity.java
@@ -10,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn0,btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btnMinus,btnDivide,btnMult,btnPlus,
+        btn0 = findViewById(R.id,btn0)
+        btn1 = findViewById(R.id,btn1)
+        btn2 = findViewById(R.id,btn2)
+        btn3 = findViewById(R.id,btn3)
+        btn4 = findViewById(R.id,btn4)
+        btn5 = findViewById(R.id,btn5)
+        btn6 = findViewById(R.id,btn6)
+        btn7 = findViewById(R.id,btn7)
+        btn8 = findViewById(R.id,btn8)
+        btn9 = findViewById(R.id,btn9)
+
+
     }
 }
